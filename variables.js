284011886e26f60849dc375a3fabcb270037c049
Anand Malani <43986351+Anandmalani@users.noreply.github.com>
2024-03-21 10:50:44 +0530

    initial commit

---
+++ variables.js
@@ -0,0 +1,19 @@
+const accountId=22344;
+let b="asbjx";
+var c="snjc";
+d="snjcjsn";
+
+// accountId=387923;// not allowed
+b=89923;
+c="sajbxb";
+d="sxjbh";
+console.table([accountId,b,c,d]);
+
+//scope
+
+{
+
+}
+/*
+
+*/
